8a982a599397dea15de0481af240da15c0c63541
xt <118093396@qq.com>
2018-01-26 09:40:37 +0800

    no message

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,6 +1,6 @@
 export default {
   host: '/api',
   wcApi: '/wcApi',
-  socketHost: 'http://127.0.0.1:8009'
-  //SocketHost: 'http://118.31.19.0:8009'
+  //socketHost: 'http://127.0.0.1:8009'
+  socketHost1: 'http://118.31.19.0:8009'
 }
